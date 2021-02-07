22fc6c31f31cd0c1277faee1e7803fe315b0f519
Afara Seun <seunvigo@gmail.com>
2021-02-07 17:23:22 +0100

    Auth route fixed fullstack

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -1,124 +1,99 @@
 const express = require('express');
 const router = express.Router();
+
+const authController = require('../../../controllers/auth');
+
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 require('dotenv/config');
 const key = process.env.APP_SECRET;
 const User = require('../../../models/User');
+const { check, body } = require('express-validator');
 
 /**
- * @route POST api/users/register
+ * @route POST api/${version}/auth/register
  * @desc Register the user
  * @access Public
  */
 
-router.post('/register', (req, res) => {
-   let { name, username, email, password, confirm_password } = req.body;
-
-   if (!name || !username || !email || !password || !confirm_password) {
-      return res.status(400).json({
-         message: 'Fill All Required Fields',
-      });
-   }
-
-   if (password !== confirm_password) {
-      return res.status(400).json({
-         message: 'Passwords do not match',
-      });
-   }
-
-   // Check for unique username
-   User.findOne({ username: username }).then((user) => {
-      if (user) {
-         return res.status(400).json({
-            message: 'Username is already taken',
-         });
-      }
-   });
-
-   // Check for unique email
-   User.findOne({ email: email }).then((user) => {
-      if (user) {
-         return res.status(400).json({
-            message: 'Email is already registered. Please login',
-         });
-      }
-   });
-
-   // The data is valid and now we can register user
-   let newUser = new User({
-      name,
-      username,
-      password,
-      email,
-   });
-   // Hash the password
-   bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-         if (err) throw err;
-         newUser.password = hash;
-         newUser.save().then((user) => {
-            return res.status(200).json({
-               success: true,
-               message: 'User is now registered.',
+router.post(
+   '/register',
+   [
+      check('username')
+         .not()
+         .isEmpty()
+         .withMessage('Username is required')
+         .trim()
+         .custom((value, { req }) => {
+            return User.findOne({ username: req.body.username }).then(
+               (userDoc) => {
+                  if (userDoc) {
+                     return Promise.reject('Username already in use');
+                  }
+               }
+            );
+         }),
+      check('email')
+         .not()
+         .isEmpty()
+         .isEmail()
+         .trim()
+         .withMessage('Please enter a valid email')
+         .custom((value, { req }) => {
+            // console.log('value', value);
+            return User.findOne({ email: req.body.email }).then((userDoc) => {
+               if (userDoc) {
+                  return Promise.reject('E-mail already in use');
+               }
             });
-         });
-      });
-   });
-});
+         })
+         .normalizeEmail(),
+      check('password')
+         .not()
+         .isEmpty()
+         .trim()
+         .withMessage('Please enter a password'),
+      check('confirm_password')
+         .not()
+         .isEmpty()
+         .trim()
+         .withMessage('Please enter passowrd again'),
+      body('confirm_password')
+         .custom((value, { req }) => {
+            if (value !== req.body.password) {
+               // console.log('value', value);
+               throw new Error('Password has to be matched!');
+            } else {
+               return true;
+            }
+         })
+         .trim(),
+   ],
+   authController.register
+);
 
 /**
- * @route POST api/users/login
+ * @route POST api/${version}/auth/login
  * @desc Login the user
  * @access Public
  */
-router.post('/login', (req, res) => {
-   User.findOne({ username: req.body.username }).then((user) => {
-      if (!req.body.password || !req.body.username) {
-         return res.status(400).json({
-            message: 'Please fill all fields',
-            success: false,
-         });
-      }
-      if (!user) {
-         return res.status(404).json({
-            message: 'Username not found',
-            success: false,
-         });
-      }
-
-      // If there is user we compare the password
-      bcrypt.compare(req.body.password, user.password).then((isMatch) => {
-         if (isMatch) {
-            // User's password is correct and we need to send the JSON Token for user
-            const payload = {
-               _id: user._id,
-               username: user.username,
-               name: user.name,
-               email: user.email,
-            };
-            //omit user password
-            var userObj = user.toObject();
-            delete userObj.password;
-            //end
-            jwt.sign(payload, key, { expiresIn: 604800 }, (err, token) => {
-               res.status(200).json({
-                  success: true,
-                  user: userObj,
-                  token: `Bearer ${token}`,
-                  message: 'You are now logged in',
-               });
-            });
-         } else {
-            return res.status(404).json({
-               message: 'Incorrect password',
-               success: false,
-            });
-         }
-      });
-   });
-});
+router.post(
+   '/login',
+   [
+      check('username')
+         .not()
+         .isEmpty()
+         .trim()
+         .withMessage('Please enter your username'),
+      check('password')
+         .not()
+         .isEmpty()
+         .trim()
+         .withMessage('Please enter a password'),
+   ],
+   authController.login
+);
 
 /**
  * @route POST api/users/profile
